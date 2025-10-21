5e5eeac9872cbae91096f273f7be4ed9e940eb0e
kstroobants <stroobants.kristof@hotmail.com>
2025-10-21 13:30:02 +0700

    fix: add optional

--- tests/integration/icontracts/schemas/call_contract_function.py
+++ tests/integration/icontracts/schemas/call_contract_function.py
@@ -14,7 +14,7 @@ call_contract_function_response = {
                 "mode": str,
                 "node_config": {
                     "address": str,
-                    "private_key": str,
+                    "private_key": Optional[str],
                     "stake": int,
                     "primary_model": {
                         "config": dict,
@@ -22,7 +22,7 @@ call_contract_function_response = {
                         "provider": str,
                         "plugin": str,
                         "plugin_config": dict,
-                        "fallback_validator": str,
+                        "fallback_validator": Optional[str],
                     },
                     "secondary_model": Optional[dict],
                 },
