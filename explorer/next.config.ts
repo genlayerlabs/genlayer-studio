e7235ad4f5ade47a4c7610733716cb79d48af393
Cristiam Da Silva <cristiam86@gmail.com>
2026-01-12 01:29:02 +0100

    fix: removed studio explorer base path

--- explorer/next.config.ts
+++ explorer/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/explorer",
-  assetPrefix: "/explorer",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
